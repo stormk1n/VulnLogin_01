a870e04859f004dce247536c24f49afc60bfd120
lahfenbrandyf <lahfenbrandy@gmail.com>
2026-05-09 11:08:31 +0100

    working on handling sessions

--- name_password_checker.py
+++ name_password_checker.py
@@ -1,13 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, session
 import base64
+import secrets
 
 app = Flask(__name__)
+
+# Needed for flask sessions
+app.secret_key = secrets.token_hex(32)
+
 @app.route("/login", methods=["GET", "POST"])
 
+
+#Defines the login request/response
 def login():
     if request.method == "POST":
         name = request.form.get("name")
-        password = request.form.get("password")
+        password = request.form.get("password") #Captures the creds from the user
         
         if not password:
             return "Password required", 400
@@ -15,13 +22,16 @@ def login():
         if len(password) < 8:
             return "Password too short!", 400
         
-        combinedCreds = f"{name}:{password}"
-        encodedCreds = base64.b46encode(combinedCreds.encode())
-        
-        return f"welcome {name}"
+        combinedCreds = f"{name}:{password}" #combines two variables for later B64 encoding
+        encodedCreds = base64.b64encode(combinedCreds.encode()).decode()
 
+        #Stores user sessions at the browser level 
         session["user"] = name
+        session["auth"] = encodedCreds 
 
+        return redirect("/dashboard")
+
+    #Creates the form
     return """
     <form method="POST">
     <input type="text" name="name" placeholder="name">
@@ -29,6 +39,25 @@ def login():
     <button type="submit">Login</button>
     </form>
     """
+
+@app.route("/dashboard")
+def dashboard():
+    if "user" not in session:
+        return "Unauthorized", 401
+
+    return f"Welcome {session['user']}"
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return "Logged out"
+
+
+#Check sessions that are on
+@app.route("/sessions")
+def sessions():
+    return dict(session)    
 
 if __name__ == "__main__":
     app.run(debug=True)
